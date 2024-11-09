117d6ecfeb9b334c13cc361ef229e3d508ee03c9
Student-details <sayeedafatimaj@gmail.com>
2024-11-09 13:28:07 +0530

    Update GitHub Actions workflow for better deployment handling

--- travel_wapp/settings.py
+++ travel_wapp/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+# Initialize environment variables
+env = environ.Env()
+environ.Env.read_env()  # This reads variables from .env if it exists
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,24 +82,42 @@ WSGI_APPLICATION = 'travel_wapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if env('USE_POSTGRESQL', default=False):  # Check for environment flag
+    DATABASE_URL = os.getenv('DATABASE_URL')
+    DATABASES = {
+        'default': env.db(DATABASE_URL)  # Automatically uses DATABASE_URL from .env file
     }
-}
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'travelwebapp',
-        'USER': 'azuresuperuser',
-        'PASSWORD': 'admin@12345',
-        'HOST': 'travelwebapp.postgres.database.azure.com',
-        'PORT': '5432',
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'travelwebapp',
+#         'USER': 'azuresuperuser',
+#         'PASSWORD': 'admin@12345',
+#         'HOST': 'travelwebapp.postgres.database.azure.com',
+#         'PORT': '5432',
+#     }
+# }
+
+# DATABASE_URL = os.getenv('DATABASE_URL')
+
+# DATABASES = {
+#     'default': env.db(DATABASE_URL)  # Automatically uses DATABASE_URL from .env
+# }
 
 
 # Password validation
